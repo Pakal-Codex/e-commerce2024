ee6c6d6651e26ef97fae3a7c4822c382c6299ee4
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2024-04-06 15:42:06 -0600

    routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory/>}/>
+          <Route path='/womens' element={<ShopCategory/>}/>
+          <Route path='/kids' element={<ShopCategory/>}/>
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
