6b251326e7ef1e595fdec5bb950afd610064c09d
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2024-04-06 17:26:43 -0600

    fixed routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
@@ -13,11 +17,14 @@ function App() {
           <Route path='/mens' element={<ShopCategory category="men"/>}/>
           <Route path='/womens' element={<ShopCategory category="women"/>}/>
           <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-          <Route path='/product' element={<Product/>}/>
-            <Route path=':productID' element={<Product/>}/>
+
+          <Route path="/product" element={<Product/>}> 
+            <Route path=':productId' element={<Product/>}/>  
+            </Route>
+          <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/> 
+        
         </Routes>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<LoginSignup/>}/>
       </BrowserRouter> 
     </div>
   );
