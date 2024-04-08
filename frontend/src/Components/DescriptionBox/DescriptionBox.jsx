9f556415a3a55fdfbd4969da0781204b84eb9a70
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2024-04-07 20:38:33 -0600

    Description Box, Realted Products Components

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>fdsgffggggggggggggggggggggggggggggggggggg
+            gfgggggggggggggggggggggggggggggggggfgfg
+            gfffffffffffffffffffffffffffffff
+            gffffffffffffffffffffggggggggggggggggg
+            gfffffffffffffffffffffffffffffff
+        </p>
+        <p>fdsgf4555555555ggggggggggggggggg
+            gfffffffffffffffffffffffffffffff
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
